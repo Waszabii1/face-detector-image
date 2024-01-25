8fff01234de815a63ecad487a57a1b06493b20a4
Waszabii1 <will.grenyer@hotmail.co.uk>
2024-01-25 21:05:31 +0000

    Update face detector image.py

--- face detector image.py
+++ face detector image.py
@@ -1,16 +1,17 @@
 import cv2
 from deepface import DeepFace
-import numpy as np
 
-imgpath = "C:\\Users\\mario\\Coding_Projeccts\\face detector image\\pictures\\download.jpg"
+imgpath = "XXXXX"
 
 image = cv2.imread(imgpath)
 
-age = DeepFace.analyze(image, actions=["age"])
-print(age)
-gender = DeepFace.analyze(image, actions=["gender"])
-print(gender[0]["dominant_gender"])
-emotion = DeepFace.analyze(image, actions=["emotion"])
-print(emotion[0]["dominant_emotion"])
-race = DeepFace.analyze(image, actions=["race"])
-print(race[0]["dominant_race"])
+age_pred = DeepFace.analyze(image, actions=["age"],)
+age = age_pred[0].get("age")
+gender_pred = DeepFace.analyze(image, actions=["gender"])
+gender = (gender_pred[0]["dominant_gender"])
+emotion_pred = DeepFace.analyze(image, actions=["emotion"])
+emotion = (emotion_pred[0]["dominant_emotion"]).capitalize()
+race_pred = DeepFace.analyze(image, actions=["race"])
+race = (race_pred[0]["dominant_race"]).capitalize()
+
+print(f"Age: {age}, \nGender: {gender}, \nEmotion: {emotion}, \nRace: {race}.")
